5a4ab73f2e57ccfde0287810e67fe8ed8b15c1b3
y1z <whyis2cool@gmail.com>
2021-09-07 04:54:36 -0500

    Make constants public .

--- src/pixel_buffer.rs
+++ src/pixel_buffer.rs
@@ -1,4 +1,13 @@
 use crate::color::RGB;
-struct PixelBuffer<T> {
+pub struct PixelBuffer<T> {
   buffer: Vec<T>,
+  width: u32,
+  height: u32,
+  ratio: [u32; 2],
 }
+
+impl<T> PixelBuffer<T> {
+  pub fn add_element(element: T) {}
+}
+
+type PixelBufferRGB = PixelBuffer<RGB>;
