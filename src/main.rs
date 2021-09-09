8d2c53740c3cd16ab4d00fb94ded17d20e043373
y1z <whyis2cool@gmail.com>
2021-09-09 02:49:46 -0500

    Save both pattern in sperate files.

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,73 @@
 mod color;
+mod make_patterns;
 mod pixel_buffer;
-use pixel_buffer::PixelBuffer;
+mod yy_random_func;
+use make_patterns::make_checker_pattern;
+use pixel_buffer::PixelBufferRGB;
 use std::fs::File;
 use std::io::{BufWriter, Write};
+use std::string::*;
 
 const DEFAULT_PATH_PPM: &'static str = "default_output.ppm";
 const DEFAULT_PATH_PAM: &'static str = "default_output.pam";
-const DEFAULT_WIDTH: usize = 16;
-const DEFAULT_HEIGHT: usize = 16;
-const DEFAULT_SIZE: usize = DEFAULT_WIDTH * DEFAULT_HEIGHT;
 
 fn main() {
-  let mut new_buffer =
-    pixel_buffer::PixelBufferRGB::create(Some(color::RGB::WHITE), 128, 128, None);
+  let mut buffer = PixelBufferRGB::create(Some(color::RGB::WHITE), 128, 128, None);
 
-  let width = new_buffer.get_width() as usize;
-  let height = new_buffer.get_height() as usize;
-  let mut mut_slice = new_buffer.get_buffer_as_slice_mut();
-  make_checker_pattern(
-    &mut mut_slice,
-    width,
-    height,
-    color::RGB::BLUE,
-    color::RGB::BLACK,
+  let buffer_width = buffer.get_width() as usize;
+  let buffer_height = buffer.get_height() as usize;
+  {
+    let slice = buffer.get_buffer_as_slice_mut();
+    make_patterns::make_random_rainbow_pattern(slice, buffer_width, buffer_height);
+  }
+
+  let rainbow_pattern_result = save_as_ppm(
+    buffer.get_width() as usize,
+    buffer.get_height() as usize,
+    buffer.get_buffer_as_slice(),
+    String::from("rainbow_pattern.ppm"),
+    true,
   );
-  println!("saving image");
-  let result = save_as_ppm(
-    new_buffer.get_width() as usize,
-    new_buffer.get_height() as usize,
-    new_buffer.get_buffer_as_slice(),
+
+  {
+    let mut mut_slice = buffer.get_buffer_as_slice_mut();
+    make_checker_pattern(
+      &mut mut_slice,
+      buffer_width,
+      buffer_height,
+      color::RGB::BLUE,
+      color::RGB::BLACK,
+    );
+  }
+  let checker_pattern_result = save_as_ppm(
+    buffer.get_width() as usize,
+    buffer.get_height() as usize,
+    buffer.get_buffer_as_slice(),
+    String::from("checker_pattern.ppm"),
+    true,
   );
-  if result.is_err() {
-    panic!("An error occurred when saving to a file");
+
+  if checker_pattern_result.is_err() {
+    panic!("An error occurred when saving the checker pattern");
+  }
+  if rainbow_pattern_result.is_err() {
+    panic!("An error occurred when saving the rainbow pattern");
   }
 }
 
-pub fn save_as_ppm(width: usize, height: usize, color: &[color::RGB]) -> std::io::Result<()> {
-  let mut file = File::create(DEFAULT_PATH_PPM)?;
+pub fn save_as_ppm(
+  width: usize,
+  height: usize,
+  color: &[color::RGB],
+  path: String,
+  be_silent: bool,
+) -> std::io::Result<()> {
+  let mut file = File::create(&path)?;
   write!(file, "P6 {} {} 255\n", width, height)?;
+
+  if be_silent {
+    println!("saving file to path {}", path);
+  }
 
   let mut writer = BufWriter::new(file);
   for y in 0..height {
@@ -52,23 +82,4 @@ pub fn save_as_ppm(width: usize, height: usize, color: &[color::RGB]) -> std::io
     }
   }
   Ok(())
-}
-
-pub fn make_checker_pattern(
-  buffer_output: &mut [color::RGB],
-  width: usize,
-  height: usize,
-  forground_color: color::RGB,
-  background_color: color::RGB,
-) {
-  for y in 0..height {
-    let indice_to_draw_at = (y % 2 == 0) as usize;
-    for x in 0..width {
-      if x % 2 == indice_to_draw_at {
-        buffer_output[(width * y) + x] = forground_color;
-      } else {
-        buffer_output[(width * y) + x] = background_color;
-      }
-    }
-  }
 }
