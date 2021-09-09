651da5a11d36307c0d34e8fcbd119ed0978f0043
y1z <whyis2cool@gmail.com>
2021-09-09 02:50:36 -0500

    Added random rainbow pattern.

--- src/make_patterns.rs
+++ src/make_patterns.rs
@@ -1,4 +1,5 @@
 use crate::color;
+use crate::yy_random_func;
 pub fn make_checker_pattern(
   buffer_output: &mut [color::RGB],
   width: usize,
@@ -14,6 +15,31 @@ pub fn make_checker_pattern(
       } else {
         buffer_output[(width * y) + x] = background_color;
       }
+    }
+  }
+}
+
+pub fn make_random_rainbow_pattern(buffer_output: &mut [color::RGB], width: usize, height: usize) {
+  const COLOR_BUFFER_COUNT: usize = 8;
+  const COLOR_BUFFER: [color::RGB; COLOR_BUFFER_COUNT] = [
+    color::RGB::BLUE,
+    color::RGB::RED,
+    color::RGB::GREEN,
+    color::RGB::CYAN,
+    color::RGB::MAGENTA,
+    color::RGB::YELLOW,
+    color::RGB::PINK,
+    color::RGB::PURPLE,
+  ];
+  let delta_between_ranges = (u32::MAX as usize) / COLOR_BUFFER.len();
+  for y in 0..height {
+    for x in 0..width {
+      let rand_num = unsafe { yy_random_func::msws32_next() };
+      let mut index = rand_num as usize / delta_between_ranges;
+      if index >= COLOR_BUFFER_COUNT {
+        index = COLOR_BUFFER_COUNT - 1;
+      }
+      buffer_output[(width * y) + x] = COLOR_BUFFER[index];
     }
   }
 }
