0d36915d2bb8d2303acec1dcebc02b583e76c011
y1z <whyis2cool@gmail.com>
2021-09-09 09:06:13 -0500

    Remove unnecessary constante value.

--- src/make_patterns.rs
+++ src/make_patterns.rs
@@ -20,8 +20,7 @@ pub fn make_checker_pattern(
 }
 
 pub fn make_random_rainbow_pattern(buffer_output: &mut [color::RGB], width: usize, height: usize) {
-  const COLOR_BUFFER_COUNT: usize = 8;
-  const COLOR_BUFFER: [color::RGB; COLOR_BUFFER_COUNT] = [
+  const COLOR_BUFFER: [color::RGB; 8] = [
     color::RGB::BLUE,
     color::RGB::RED,
     color::RGB::GREEN,
@@ -31,15 +30,21 @@ pub fn make_random_rainbow_pattern(buffer_output: &mut [color::RGB], width: usiz
     color::RGB::PINK,
     color::RGB::PURPLE,
   ];
-  let delta_between_ranges = (u32::MAX as usize) / COLOR_BUFFER.len();
+  let color_buffer_len = COLOR_BUFFER.len();
+  let delta_between_ranges = (u32::MAX as usize) / color_buffer_len;
   for y in 0..height {
     for x in 0..width {
       let rand_num = unsafe { yy_random_func::msws32_next() };
       let mut index = rand_num as usize / delta_between_ranges;
-      if index >= COLOR_BUFFER_COUNT {
-        index = COLOR_BUFFER_COUNT - 1;
+      if index >= color_buffer_len {
+        index = color_buffer_len - 1;
       }
       buffer_output[(width * y) + x] = COLOR_BUFFER[index];
     }
   }
+}
+
+// TODO : implement
+pub fn make_triangle_pattern(buffer_output: &mut [color::RGB], width: usize, height: usize) {
+  panic!("not implemented");
 }
