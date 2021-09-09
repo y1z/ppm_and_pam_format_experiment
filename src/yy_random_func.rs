b3fc804e7619d0fad76b5aff4d54dc59a7034a59
y1z <whyis2cool@gmail.com>
2021-09-09 01:37:32 -0500

    Make everything wrapping to avoid panic call

--- src/yy_random_func.rs
+++ src/yy_random_func.rs
@@ -1,19 +1,21 @@
+use std::num::Wrapping;
+use std::ops::{BitOr, Shl, Shr};
 // Implementation of middle square weyl sequence
 // Based on this video : https://youtu.be/VHJUlRiRDCY?t=2045
 // Rules for this constants
-const RANDOM_CONST: u64 = 0x78E364C1_2679FAB3 as u64;
+const RANDOM_CONST: u64 = 0x78E364C1_2679FAB3;
 // 1. Has to be on odd number
 // 2. Every 4-bytes should have unique nibble with in those bytes, aka no repretied value in each halve
 //    no value like 0xff 0xee 0xcc 0xbb 0xaa etc...
 // 3. No nibble should equal zero
 
 pub unsafe fn msws32_next() -> u32 {
-  static mut X_WEYL32: u64 = 0u64;
-  static mut W_WEYL32: u64 = 0u64;
+  static mut X_WEYL32: u64 = 0u64 as u64;
+  static mut W_WEYL32: u64 = 0u64 as u64;
 
-  let square = X_WEYL32 * X_WEYL32;
-  let random_const = RANDOM_CONST;
-  W_WEYL32 = W_WEYL32 + random_const;
-  X_WEYL32 = square + W_WEYL32;
-  return ((X_WEYL32 >> 32) | (X_WEYL32 << 32)) as u32;
+  let square = X_WEYL32.wrapping_mul(X_WEYL32);
+  W_WEYL32 = W_WEYL32.wrapping_add(RANDOM_CONST);
+  X_WEYL32 = X_WEYL32.wrapping_add(W_WEYL32.wrapping_add(square));
+  let shifted_x = X_WEYL32.wrapping_shl(32) as u32 | X_WEYL32.wrapping_shr(32) as u32;
+  return shifted_x;
 }
