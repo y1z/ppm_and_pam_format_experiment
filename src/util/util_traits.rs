1f2577fa10056bc25a49bfcd622c4bd4118961a4
y1z <whyis2cool@gmail.com>
2021-09-09 19:11:28 -0500

    Make sure f32 and f64 satifys the 'BasicArithmetic' trait.

--- src/util/util_traits.rs
+++ src/util/util_traits.rs
@@ -90,7 +90,7 @@ macro_rules! IMPL_BASIC_ARITHMETIC{
     }
   )*)
 }
-IMPL_BASIC_ARITHMETIC! {i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize }
+IMPL_BASIC_ARITHMETIC! {i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize f32 f64 }
 
 ///
 /// Implementation of traits for primitive types
