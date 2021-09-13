6a2ed354f4cb3af425895cb24ce518a5bdb51217
y1z <whyis2cool@gmail.com>
2021-09-12 20:13:01 -0500

    Now only makes the triangle pattern

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 mod color;
 mod make_patterns;
 mod pixel_buffer;
+mod vector2;
 mod yy_random_func;
+pub mod util {
+  pub mod util_traits;
+}
+
 use make_patterns::make_checker_pattern;
 use pixel_buffer::PixelBufferRGB;
 use std::fs::File;
@@ -12,47 +17,64 @@ const DEFAULT_PATH_PPM: &'static str = "default_output.ppm";
 const DEFAULT_PATH_PAM: &'static str = "default_output.pam";
 
 fn main() {
-  let mut buffer = PixelBufferRGB::create(Some(color::RGB::WHITE), 128, 128, None);
+  let mut buffer = PixelBufferRGB::create(Some(color::RGB::WHITE), 12, 12, None);
 
   let buffer_width = buffer.get_width() as usize;
   let buffer_height = buffer.get_height() as usize;
-  {
-    let slice = buffer.get_buffer_as_slice_mut();
-    make_patterns::make_random_rainbow_pattern(slice, buffer_width, buffer_height);
-  }
+  //{
+  //  let slice = buffer.get_buffer_as_slice_mut();
+  //  make_patterns::make_random_rainbow_pattern(slice, buffer_width, buffer_height);
+  //}
 
-  let rainbow_pattern_result = save_as_ppm(
-    buffer.get_width() as usize,
-    buffer.get_height() as usize,
-    buffer.get_buffer_as_slice(),
-    String::from("rainbow_pattern.ppm"),
-    true,
-  );
+  //let rainbow_pattern_result = save_as_ppm(
+  //  buffer.get_width() as usize,
+  //  buffer.get_height() as usize,
+  //  buffer.get_buffer_as_slice(),
+  //  String::from("rainbow_pattern.ppm"),
+  //  true,
+  //);
+
+  //{
+  //  let mut mut_slice = buffer.get_buffer_as_slice_mut();
+  //  make_checker_pattern(
+  //    &mut mut_slice,
+  //    buffer_width,
+  //    buffer_height,
+  //    color::RGB::BLUE,
+  //    color::RGB::BLACK,
+  //  );
+  //}
+  //let checker_pattern_result = save_as_ppm(
+  //  buffer.get_width() as usize,
+  //  buffer.get_height() as usize,
+  //  buffer.get_buffer_as_slice(),
+  //  String::from("checker_pattern.ppm"),
+  //  true,
+  //);
 
   {
     let mut mut_slice = buffer.get_buffer_as_slice_mut();
-    make_checker_pattern(
+    make_patterns::make_triangle_pattern(
       &mut mut_slice,
       buffer_width,
       buffer_height,
       color::RGB::BLUE,
-      color::RGB::BLACK,
     );
   }
   let checker_pattern_result = save_as_ppm(
     buffer.get_width() as usize,
     buffer.get_height() as usize,
     buffer.get_buffer_as_slice(),
-    String::from("checker_pattern.ppm"),
+    String::from("triangle_pattern.ppm"),
     true,
   );
 
-  if checker_pattern_result.is_err() {
-    panic!("An error occurred when saving the checker pattern");
-  }
-  if rainbow_pattern_result.is_err() {
-    panic!("An error occurred when saving the rainbow pattern");
-  }
+  //if checker_pattern_result.is_err() {
+  //  panic!("An error occurred when saving the checker pattern");
+  //}
+  //if rainbow_pattern_result.is_err() {
+  //  panic!("An error occurred when saving the rainbow pattern");
+  //}
 }
 
 pub fn save_as_ppm(
