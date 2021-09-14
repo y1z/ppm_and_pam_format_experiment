feaa6ee9bbfd6a58b34968426b9cb7e929cf3a0b
y1z <whyis2cool@gmail.com>
2021-09-14 13:36:31 -0500

    Add struct for describing how to format a .pam image

---
+++ src/pam_format_descriptor.rs
@@ -0,0 +1,29 @@
+use std::fmt;
+#[repr(u32)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum TupleType {
+  BLACKANDWHITE,
+  GRAYSCALE,
+  RGB,
+  ALPHA = (1 << 16),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub struct PamFormatDescriptor {
+  pub width: usize,
+  pub height: usize,
+  pub tuple_type: TupleType,
+  pub depth: u16,
+  pub max_val: u8,
+}
+
+impl fmt::Display for TupleType {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    let raw_value: u32 = (*self) as u32;
+    let has_alpha: bool = raw_value & (1 << 16) > 0;
+    if has_alpha {
+      write!(f, "{:?}_ALPHA", self);
+    }
+    write!(f, "{:?}", self)
+  }
+}
